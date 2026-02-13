7971c56e1827df6f0a3e9133ee37c5de17c649a0
msaririzki <msaririzki12@gmail.com>
2026-02-13 09:51:01 +0800

    Fix HTTPS: deteksi dari APP_URL bukan APP_ENV agar kompatibel dengan server lokal

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,8 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // Force HTTPS di production (di balik Nginx proxy)
-        if ($this->app->environment('production')) {
+        // Force HTTPS jika APP_URL menggunakan https
+        if (str_starts_with(config('app.url'), 'https://')) {
             URL::forceScheme('https');
         }
 
