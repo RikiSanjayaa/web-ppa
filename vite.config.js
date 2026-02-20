ef40fb763424409222b02bb37ebafe47077b2d6d
msaririzki <msaririzki12@gmail.com>
2026-02-20 11:08:01 +0800

    fix: hapus file sampah dan perbaiki vite host config

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
         }),
     ],
     server: {
-        host: '127.0.0.1',
+        host: 'localhost',
         port: 5173,
         hmr: {
             host: 'localhost',
